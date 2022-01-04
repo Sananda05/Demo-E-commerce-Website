7fae84955f44b9961c6f3cdeb978bdeff882606c
Sananda <sananda@iut-dhaka.edu>
2022-01-04 14:59:41 +0600

    created separate app for homepage

---
+++ PandaMart/HomePage/urls.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+
+from PandaMart.settings import STATIC_URL
+
+from . import views
+
+urlpatterns = [
+   path('/homepage', views.homePageView),
+]
+
+if settings.DEBUG:
+
+        urlpatterns+= static(settings.STATIC_URL,document_root= settings.STATIC_ROOT)
+       
--- PandaMart/PandaMart/urls.py
+++ PandaMart/PandaMart/urls.py
@@ -5,5 +5,6 @@ from django.urls import path, include
 urlpatterns = [
     path ('', include('Authentication.urls')),
     path('admin/', admin.site.urls),
+    path('', include('HomePage.urls'))
   
 ]
