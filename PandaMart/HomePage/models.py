0ee427eabab3ef7b468b2746668dfed3ad30d178
Sananda <sananda@iut-dhaka.edu>
2022-01-04 16:20:02 +0600

    created model for product

--- PandaMart/HomePage/models.py
+++ PandaMart/HomePage/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class ProductInfo(models.Model):
+
+    product_name = models.CharField(max_length=100)
+    price = models.CharField(max_length=100)
+    view = models.CharField(max_length=100)
+    quantity = models.CharField(max_length=100)
+    
